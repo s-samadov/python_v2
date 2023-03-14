2d2299d3dc5c8db5bd3193d7de7fc73caca15784
s.samadov <sz.samadov@gmail.com>
2023-03-14 11:45:26 +0500

    add task003

---
+++ Practice_1/task003.py
@@ -0,0 +1,11 @@
+a = int(input('Enter count of student in class A: '))
+b = int(input('Enter count of student in class B: '))
+c = int(input('Enter count of student in class C: '))
+count_a = (a + 1) // 2
+count_b = (b + 1) // 2
+count_c = (c + 1) // 2
+count  = count_a + count_b + count_c
+print(count)
+
+
+
