9b74904031e17458094973eba26adaa2881955b0
s.samadov <sz.samadov@gmail.com>
2023-03-14 13:10:02 +0500

    add task005

---
+++ Practice_1/task005.py
@@ -0,0 +1,7 @@
+i = int(input("Enter number in carriage: "))
+j = int(input("Enter number on carriage: "))
+if i == j:
+    print("Шt is not possible to count the number of train carriage")
+else:
+    count = i + j - 1
+print(f"Count of train carriage {count}")  
