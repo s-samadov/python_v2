31a2e1a161dc1cc32a1bfbfe80bba1f4357f781f
s.samadov <sz.samadov@gmail.com>
2023-03-14 10:29:57 +0500

    add Lecture_1

---
+++ Lecture_1/main001.py
@@ -0,0 +1,52 @@
+# numb = 5
+# word = "hello"
+# numb_2 = 2.5
+# print(f"{numb} - {numb_2} - {word}")
+# print("{} - {} - {}".format(numb, numb_2, word))
+
+# print("Enter the first number: ")
+# a = int(input())
+# b = int(input("Enter the second number: "))
+# print(a, ' + ', b, ' = ', a + b)
+
+# a = 5.029384
+# b = 6.623434
+# print(round(a * b, 3))
+
+# i = 3
+# i += 3  # i = i + 3
+# i -= 3  # i = i - 3
+# i *= 3  # i = i * 3
+# i /= 3  # i = i / 3
+# i //= 3 # i = i // 3
+# i %= 3  # i = i % 3
+# i **= 3 # i = i ** 3
+
+# a = 'qwerty'
+# print(a[0])
+# print(a[3])
+# print()
+
+# for i in a:
+#     print(i)
+
+# text = 'Just do it!'
+# print(len(text))
+# print('do' in text)
+# print(text.lower())
+# print(text.upper())
+# print(text.replace('do', 'bitch'))
+
+# text = 'Just do it!'
+# print(text[0])
+# print(text[len(text) - 1])
+# print(text[-3])
+# print(text[:])
+# print(text[:6])
+# print(text[2:6])
+# print(text[len(text) - 5:])
+# print(text[2:-5])
+# print(text[0:len(text):2])
+# print(text[::2])
+# text = text[1:3] + text[-2] + text[:5]
+# print(text)
